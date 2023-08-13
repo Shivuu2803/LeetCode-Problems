60085b7c8e9d153b077a57de74458606bd19706b
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-08-13 23:39:37 +0530

    Create 27-Find the Index of the First Occurrence in a String.cpp

---
+++ 27-Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    int strStr(string haystack, string needle) 
+    {
+        int m = haystack.size();
+        int n = needle.size();
+
+        if (n == 0)
+        {
+            return 0; 
+        }
+        
+        if (n > m)
+        {
+            return -1;
+        }
+        
+        for (int i = 0; i <= m - n; i++) 
+        {
+            int j = 0;
+            for (; j < n; j++) 
+            {
+                if (haystack[i + j] != needle[j]) 
+                {
+                    break;
+                }
+            }
+            if (j == n)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};

d5be8700c806a7a0c062deb73829c29829654f3c
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-08-16 14:45:02 +0530

    Create 32-Excel sheet column number.cpp

---
+++ 32-Excel sheet column number.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int titleToNumber(string columnTitle) 
+    {
+        int columnNumber = 0;
+        
+        for (char c : columnTitle) 
+        {
+            columnNumber = columnNumber * 26 + (c - 'A' + 1);
+        }
+        
+        return columnNumber;   
+    }
+};

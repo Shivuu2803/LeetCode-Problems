780810136c131980cd9a41339e367cf41788e73d
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-08-20 01:46:06 +0530

    Create 39-unique character in string.cpp

---
+++ 39-unique character in string.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public:
+    int firstUniqChar(string s) 
+    {
+       vector<int> count(128, 0);
+
+       for (const char c : s)
+       {
+           ++count[c];
+       }
+
+       for (int i = 0; i < s.length(); ++i)
+       {
+           if (count[s[i]] == 1)
+           return i;
+       }
+        return -1;
+    }
+};

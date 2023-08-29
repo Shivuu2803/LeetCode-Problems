2794fc9a4cbf5bf407fea6a9a4bcb39eaa5c2978
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-08-29 15:53:46 +0530

    Create 60-sort array by parity.cpp

---
+++ 60-sort array by parity.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) 
+    {
+        int start = 0;
+        int end = nums.size() - 1;
+
+    while (start < end) 
+    {
+      if (nums[start] % 2 == 1 && nums[end] % 2 == 0)
+      {
+        swap(nums[start], nums[end]);
+      }
+      if (nums[start] % 2 == 0)
+      {
+        ++start;
+      }
+      if (nums[end] % 2 == 1)
+      {
+        --end;
+      }
+    }
+
+    return nums;
+    }
+};

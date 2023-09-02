d7b9b710b5cc3303ea04768ca24068332a50f65a
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-09-02 22:02:28 +0530

    Create 71-valid perfect square.cpp

---
+++ 71-valid perfect square.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    bool isPerfectSquare(int num) 
+    {
+        long long low = 1, high = num;
+
+        while (low <= high)
+        {
+            long long mid = (low + high) >> 1;
+            if (mid * mid == num)
+            {
+                return true;
+            }
+            else if (mid * mid < num)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
